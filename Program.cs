b4c32b17d4293c381353b2f72e5d2daf1a41c4e4
Alexandr Kazanov <kazanov.aleks@gmail.com>
2023-01-24 17:23:43 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+namespace WebApplication2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            app.Run(async (context) =>
+            {
+                var response = context.Response;
+                response.ContentType= "text/html";
+                await response.WriteAsync("<h2>Name:</h2><h3>Alexandr Kazanov</h3>\n" +
+                    $"<h2>Date:</h2><h3>{DateTime.Now}</h3>\n" +
+                    $"<h2>Environment:</h2><h3>{app.Environment.EnvironmentName}</h3>\n" +
+                    $"<h2>Application:</h2><h3>{app.Environment.ApplicationName}</h3>\n" +
+                    $"<h2>Host:</h2><h3>{response.HttpContext.Request.Host}</h3>\n" +
+                    $"<h2>Protocol:</h2><h3>{response.HttpContext.Request.Protocol}</h3>");
+            });
+
+            app.Run();
+        }
+    }
+}
